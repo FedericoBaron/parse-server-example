4f6218c994c3f3480af7895180fe03ca28b4ecda
Federico Agustin Barón <federicobaron@fb.com>
2020-08-01 20:36:23 -0400

    Test push

--- cloud/main.js
+++ cloud/main.js
@@ -27,3 +27,33 @@ Parse.Cloud.define('pingReply', function(request, response) {
 
   response.success('success');
 });
+
+
+Parse.Cloud.define("sendPushNotification", function(request, response) {
+        // var userId = request.params.userId;
+        // var message = request.params.message;
+        // var queryUser = new Parse.Query(Parse.User);
+        var userId = JSON.parse(customData).userId;
+        queryUser.equalTo('objectId', userId);
+        var query = new Parse.Query(Parse.Installation);
+        query.matchesQuery('user', queryUser);
+
+        Parse.Push.send({
+          where: query,
+          data: {
+            alert: message,
+            badge: 0,
+            sound: 'default'
+          }
+        }, {
+          success: function() {
+            console.log('##### PUSH OK');
+            response.success();
+          },
+          error: function(error) {
+            console.log('##### PUSH ERROR');
+            response.error('ERROR');
+          },
+          useMasterKey: true
+        });
+});
