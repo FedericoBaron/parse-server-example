2338b7e2b5259ebae2776b83a11230bf5e1e6191
Federico Agustin Barón <federicobaron@fb.com>
2020-08-03 17:47:17 -0400

    Push request

--- cloud/main.js
+++ cloud/main.js
@@ -31,23 +31,16 @@ Parse.Cloud.define('pingReply', function(request, response) {
 
 Parse.Cloud.define("sendPushNotification", function(request, response) {
         var params = request.params;
-        // var customData = params.customData;
-
-        // if (!customData) {
-        //   response.error("Missing customData!")
-        // }
-
-        //var userId = JSON.parse(customData).userId;
-        var userId = params.userId
+        var objectId = params.userId
         var queryUser = new Parse.Query(Parse.User);
-        queryUser.equalTo('objectId', userId);
+        queryUser.equalTo('objectId', objectId);
         var query = new Parse.Query(Parse.Installation);
         query.matchesQuery('user', queryUser);
 
         Parse.Push.send({
           where: query,
           // Parse.Push requires a dictionary, not a string.
-          data: {"alert": "WE SCORED!"},
+          data: {"alert": "You got a new request"},
           }, { success: function() {
           console.log("#### PUSH OK");
           }, error: function(error) {
@@ -55,24 +48,6 @@ Parse.Cloud.define("sendPushNotification", function(request, response) {
           }, useMasterKey: true});
 
   response.success('success');
-        // Parse.Push.send({
-        //   where: query,
-        //   data: {
-        //     alert: "message",
-        //     badge: 0,
-        //     sound: 'default'
-        //   }
-        // }, {
-        //   success: function() {
-        //     console.log('##### PUSH OK');
-        //     response.success();
-        //   },
-        //   error: function(error) {
-        //     console.log('##### PUSH ERROR');
-        //     response.error('ERROR');
-        //   },
-        //   useMasterKey: true
-        // });
 });
 
 Parse.Cloud.define("sendPushNotificationChannel", function(request, response) {
